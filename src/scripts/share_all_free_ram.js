5e315e65496ce78453a489db060958c6369278b4
Kaijian Gao <gao_kaijian@hotmail.com>
2025-06-12 04:26:28 +0800

    Add script to share all free ram from a server to factions

---
+++ src/scripts/share_all_free_ram.js
@@ -0,0 +1,21 @@
+/** @param {NS} ns **/
+// Get all free ram on current server and kick off max threads of share.js
+export async function main(ns) {
+  const server = ns.args[0];
+
+  while (true) {
+    const maxRam = ns.getServerMaxRam(server);
+    const usedRam = ns.getServerUsedRam(server)
+    const freeRam = maxRam - usedRam;
+    const shareScriptRam = 4;
+    const maxThreads = Math.floor(freeRam / shareScriptRam);
+    ns.print(`${server} has ${freeRam} free RAM, kicking off ${maxThreads} threads of share.js`);
+
+    if (maxThreads == 0) {
+      await ns.sleep(1000);
+      continue;
+    }
+    ns.exec("kamu/share.js", server, maxThreads);
+    await ns.sleep(10025); // Sleep 25ms extra to make sure share is done running
+  }
+}
