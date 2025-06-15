4e23dd0a368585dc17712ea522fa6ecdd258521e
Kaijian Gao <gao_kaijian@hotmail.com>
2025-06-16 03:01:08 +0800

    Rename to snake case

--- src/scripts/hacknet_manager.js
+++ src/scripts/hacknet_manager.js
@@ -1,4 +1,4 @@
-import { getSafeBitNodeMultipliers } from "./bitnode-multipliers.js";
+import { getSafeBitNodeMultipliers } from "./bitnode_multipliers.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
